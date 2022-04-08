a7d475eb50b53bedccc29d5e75322c5f8d01cae0
TheRKJ <rakeshjangid96496@gmail.com>
2022-04-08 15:27:01 +0530

    refactored fib

--- 001-fibonacci/main.py
+++ 001-fibonacci/main.py
@@ -1,7 +1,9 @@
 def fib(n):
     if(n <= 2):
-        return(1)
-    return(fib(n-1) + fib(n-2))
+        answer = 1
+    else:
+        answer = fib(n-1) + fib(n-2)
+    return answer
 
 for i in range(10):
     print(fib(i+1), end=" ")
