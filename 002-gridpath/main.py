c8fed54f7239514a12417c72b2464c6d5ea77376
TheRKJ <rakeshjangid96496@gmail.com>
2022-04-09 16:35:13 +0530

    Memoization implemented in gridpath

--- 002-gridpath/main.py
+++ 002-gridpath/main.py
@@ -1,8 +1,15 @@
-def countPaths(m, n):
-    if(m == 1 or n == 1):
-        return(1)
-    return(countPaths(m, n-1) + countPaths(m-1, n))
+def countPaths(m, n, memo):
+    if (m, n) in memo:
+        return(memo[(m, n)])
+    elif(m == 1 or n == 1):
+        ans = 1
+        memo[(m, n)] = ans
+    else:
+        ans = countPaths(m, n-1, memo) + countPaths(m-1, n, memo)
+        memo[(m, n)] = ans
+    return(ans)
 
-m = 3
-n = 3
-print(countPaths(m, n))
+m = 50
+n = 50
+memo = {}
+print(countPaths(m, n, memo))
