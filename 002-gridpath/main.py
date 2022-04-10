fc27057bee8105dabc410f87348cae7d91d35d50
TheRKJ <rakeshjangid96496@gmail.com>
2022-04-11 00:53:28 +0530

    Path printing implemented

--- 002-gridpath/main.py
+++ 002-gridpath/main.py
@@ -1,3 +1,4 @@
+#Task-1
 def countPaths(m, n, memo):
     if (m, n) in memo:
         return(memo[(m, n)])
@@ -9,7 +10,27 @@ def countPaths(m, n, memo):
         memo[(m, n)] = ans
     return(ans)
 
-m = 50
-n = 50
+m = 3
+n = 3
 memo = {}
-print(countPaths(m, n, memo))
+print(f"Total Paths: {countPaths(m, n, memo)}")
+
+
+
+#Task-2
+def printPaths(m, n, result):
+    if(m == len(grid)-1 and n == len(grid[0])-1):
+        result += f"{grid[m][n]} "
+        print(result)
+        return
+    if(m >= len(grid) or n >= len(grid[0])):
+        return
+    result += f"{grid[m][n]} "
+    printPaths(m, n+1, result)
+    printPaths(m+1, n, result)
+
+print("Trejectories:")
+grid = [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]] 
+printPaths(0, 0, "")
